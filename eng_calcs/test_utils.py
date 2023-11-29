ecfcff599be71b079e2ce5ddafadf5773038a44e
Dazzledorf <darryll.shanks@gmail.com>
2023-11-29 23:16:47 +1000

    Update test_utils.py

--- eng_calcs/test_utils.py
+++ eng_calcs/test_utils.py
@@ -1,4 +1,5 @@
-import utils
+import math
+from eng_calcs import utils
 
 
 def test_str_to_int():
@@ -21,3 +22,13 @@ def test_str_to_float():
     assert utils.str_to_float(test_str_2) == -2000.0
     assert utils.str_to_float(test_str_3) == 324.625
     assert utils.str_to_float(test_str_4) == 'POINT:Fy'
+
+
+def test_round_down():
+    test_1 = utils.round_down(decimals=1, n=3.142)
+    assert math.isclose(test_1, 3.1, rel_tol=1e-6, abs_tol=1e-6)
+
+
+def test_round_up():
+    test_1 = utils.round_up(decimals=1, n=3.142)
+    assert math.isclose(test_1, 3.2, rel_tol=1e-6, abs_tol=1e-6)
